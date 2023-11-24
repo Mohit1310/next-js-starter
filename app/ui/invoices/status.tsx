5e543935c927e354a87611b3df8b5c0042e1bd93
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-11-24 16:14:03 +0530

    fixed

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -2,7 +2,7 @@ import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
-  returnk (
+  return (
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
